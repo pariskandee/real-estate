8a8128c9f50fda5a2e514e05e1ec33247181cad1
kingtamil <o661291469@gmail.com>
2025-04-05 12:47:52 +0200

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHhdsL0w_3Q8dfVJBJCYXX9GD7Q3g7Fac",
-  authDomain: "nexaimmo-e7519.firebaseapp.com",
-  projectId: "nexaimmo-e7519",
-  storageBucket: "nexaimmo-e7519.firebasestorage.app",
-  messagingSenderId: "1016819299796",
-  appId: "1:1016819299796:web:bcd03d1413361134135241",
-  measurementId: "G-CSXW3PWG63"
+  
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
